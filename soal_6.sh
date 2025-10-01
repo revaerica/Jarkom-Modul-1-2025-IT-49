f5e94bd6ba603fdd64d226f96a8185c6ee5ac255
reva <reva.erica24@gmail.com>
2025-10-01 23:14:15 +0700

    Update soal_6.sh

--- soal_6.sh
+++ soal_6.sh
@@ -1,1 +1,12 @@
+apt update
+apt install wget
 
+wget --no-check-certificate "https://drive.google.com/file/d/1bE3kF1Nclw0VyKq4bL2VtOOt53IC7lG5&export=download" traffic.zip
+
+unzip traffic.zip
+
+chmod +x traffic.sh
+
+ ./traffic.sh
+
+ ping 10.88.1.1
