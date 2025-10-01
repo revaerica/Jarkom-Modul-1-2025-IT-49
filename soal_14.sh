c8cc6cce241d042d58ab63a5687065291cc2d561
reva <reva.erica24@gmail.com>
2025-10-01 22:02:43 +0700

    Create soal_14.sh

---
+++ soal_14.sh
@@ -0,0 +1,27 @@
+nc 10.15.43.32 3401
+
+#file yang diberikan diexport ke Wireshark
+
+# Isi Soal dan Jawaban dari Packag yang difilter di Wireshark
+# ===== Soal 14 =====
+# Difficulty: Easy
+# Note: You can exit anytime by typing 'exit'
+
+# How many packets are recorded in the pcapng file?
+# Format: int
+# > 500358
+
+# What are the user that successfully logged in?
+# Format: user:pass
+# > n1enna:y4v4nn4_k3m3nt4r1
+
+# In which stream were the credentials found?
+# Format: int
+# > 41824
+
+# What tools are used for brute force?
+# Format: Hydra v1.8.0-dev
+# > Fuzz Faster U Fool v2.1.0-dev
+# Congratulations! Here is your flag: KOMJAR25{Brut3_F0rc3_gMmmf2sGnd9gbyg5zth5QYTqZ}
+
+# KOMJAR25{Brut3_F0rc3_gMmmf2sGnd9gbyg5zth5QYTqZ} */
