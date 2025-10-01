68ee9ee146903997a64092750717beb0dc08a03c
reva <reva.erica24@gmail.com>
2025-10-01 23:09:55 +0700

    Update soal_8.sh

--- soal_8.sh
+++ soal_8.sh
@@ -1,1 +1,18 @@
+apt update
+apt install wget
+apt install unzip -y 
+apt install vsftpd -y
+apt install ftp -y
 
+wget --no-check-certificate "https://drive.usercontent.google.com/u/0/uc?id=11ra_yTV_adsPIXeIPMSt0vrxCBZu0r33&export=download" -O cuaca.zip
+
+unzip cuaca.zip
+
+service vsftpd start
+
+ftp 10.88.2.1
+ftp> cd shared
+ftp> put cuaca.txt
+ftp> put mendung.jpg
+
+# "sebelum itu buka dulu wireshark di kabel"
