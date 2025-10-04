5f904e1fb872bcb5d55f9eb3ecb415434a0c40c7
reva <reva.erica24@gmail.com>
2025-10-04 22:07:15 +0700

    Update soal_11.sh

--- soal_11.sh
+++ soal_11.sh
@@ -20,22 +20,4 @@ apt install netcat-traditional -y
 
 telnet 10.88.1.2 
 
-# Pemindaian Port
-# Di Eru
-
-apt-get install netcat -y 
-
-echo "--- Port 21 (FTP): ---"
-nc -v -z 10.88.1.2 21
-
-echo "--- Port 80 (HTTP): ---"
-nc -v -z 10.88.1.2 80
-
-echo "--- Port 666 (Rahasia): ---"
-nc -v -z 10.88.1.2 666
-
-# Di Varda
-apt-get install -y ssh
-ssh test@10.88.1.1 
-
 # Start capture di Wirshark
