90f7cdd94a73ffa629ee6888ad5e1163fc0ca359
reva <reva.erica24@gmail.com>
2025-10-04 21:17:32 +0700

    Update soal_11.sh

--- soal_11.sh
+++ soal_11.sh
@@ -1,27 +1,39 @@
 # Di Melkor
-apt update
-apt-get install telnetd xinetd -y
-
-cat << EOF > /etc/xinetd.d/telnet
-service telnet
-{
-    disable         = no
-    flags           = REUSE
-    socket_type     = stream
-    wait            = no
-    user            = root
-    server          = /usr/sbin/in.telnetd
-    log_on_failure  += USERID
-}
-EOF
-
-service xinetd restart
+apt-get update
+apt-get install -y telnetd inetutils-telnetd openbsd-inetd
 
 useradd -m -d /home/test -s /bin/bash test
 echo "test:test123" | chpasswd
 
-# Di Eru
-apt update
-apt-get install telnetd xinetd -y
+sed -i '/telnet/d' /etc/inetd.conf
+echo "telnet stream tcp nowait root /usr/sbin/in.telnetd /usr/sbin/login" >> /etc/inetd.conf
 
-telnet 10.88.1.2
+service openbsd-inetd restart
+
+ss -tuln | grep :23
+
+# Di Eru
+apt-get update
+apt-get install -y telnet netcat
+
+telnet 10.88.1.2 
+
+# Pemindaian Port
+# Di Eru
+
+apt-get install netcat -y 
+
+echo "--- Port 21 (FTP): ---"
+nc -v -z 10.88.1.2 21
+
+echo "--- Port 80 (HTTP): ---"
+nc -v -z 10.88.1.2 80
+
+echo "--- Port 666 (Rahasia): ---"
+nc -v -z 10.88.1.2 666
+
+# Di Varda
+apt-get install -y ssh
+ssh test@10.88.1.1 
+
+# Start capture di Wirshark
