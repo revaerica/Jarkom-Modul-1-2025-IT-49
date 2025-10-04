7eae182b2af8a9e4ebc7b9aeacce1c02282258d1
reva <reva.erica24@gmail.com>
2025-10-04 22:20:59 +0700

    Update soal_12.sh

--- soal_12.sh
+++ soal_12.sh
@@ -1,1 +1,12 @@
+# Di Eru
+apt-get update
+apt install netcat-traditional -y
 
+echo "--- Memeriksa Port 21 (FTP) ---"
+nc.traditional -v -z 10.88.1.2 21
+
+echo "--- Memeriksa Port 80 (HTTP) ---"
+nc.traditional -v -z 10.88.1.2 80
+
+echo "--- Memeriksa Port Rahasia 666 ---"
+nc.traditional -v -z 10.88.1.2 666
