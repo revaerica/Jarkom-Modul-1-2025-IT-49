dd47194eb822df52938ec9fe4c407901c2f434d5
reva <reva.erica24@gmail.com>
2025-10-04 13:38:54 +0700

    Update soal_11.sh

--- soal_11.sh
+++ soal_11.sh
@@ -1,1 +1,27 @@
+# Di Melkor
+apt update
+apt-get install telnetd xinetd -y
 
+cat << EOF > /etc/xinetd.d/telnet
+service telnet
+{
+    disable         = no
+    flags           = REUSE
+    socket_type     = stream
+    wait            = no
+    user            = root
+    server          = /usr/sbin/in.telnetd
+    log_on_failure  += USERID
+}
+EOF
+
+service xinetd restart
+
+useradd -m -d /home/test -s /bin/bash test
+echo "test:test123" | chpasswd
+
+# Di Eru
+apt update
+apt-get install telnetd xinetd -y
+
+telnet 10.88.1.2
