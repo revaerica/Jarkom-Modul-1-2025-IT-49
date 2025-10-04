8b5c6739e705f07201520fb3c9328f9a2033dc9d
reva <reva.erica24@gmail.com>
2025-10-04 22:59:48 +0700

    Update soal_13.sh

--- soal_13.sh
+++ soal_13.sh
@@ -1,3 +1,10 @@
-# Di Varda
-apt-get install -y openssh-client
+# Di Eru
+apt update
+apt-get install -y openssh-server
+
+service ssh restart
+
+ss -tuln | grep :22
+
+# Di varda
 ssh test@10.88.1.1
