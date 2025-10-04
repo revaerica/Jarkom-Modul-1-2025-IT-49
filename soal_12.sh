9219baecad340e8086e61a0debb8cf1a286070d0
reva <reva.erica24@gmail.com>
2025-10-04 22:32:11 +0700

    Update soal_12.sh

--- soal_12.sh
+++ soal_12.sh
@@ -2,6 +2,20 @@
 apt-get update
 apt install netcat-traditional -y
 
+# Di Melkor
+apt update
+apt install vsftpd -y 
+apt install nginx -y 
+
+service vsftpd start
+service nginx start
+
+sed -i '/telnet/d' /etc/inetd.conf
+echo "telnet stream tcp nowait root /usr/sbin/in.telnetd /usr/sbin/in.telnetd -L /bin/login" >> /etc/inetd.conf
+
+service openbsd-inetd restart
+
+# Di Eru
 echo "--- Memeriksa Port 21 (FTP) ---"
 nc.traditional -v -z 10.88.1.2 21
 
